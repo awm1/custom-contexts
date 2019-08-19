43d83b84cd36fcabab83e21efb1811cc2d86ab75
Jose Rivera <ing.joserivera26@gmail.com>
2019-08-19 14:44:41 -0600

    Fix audio installation

--- cc-install.sh
+++ cc-install.sh
@@ -9,7 +9,7 @@ rm -rf /var/cache/yum
 
 #Download the resources
 rm -f /var/lib/asterisk/sounds/en/your-public-ip-is.wav
-wget -P /var/lib/asterisk/sounds/en/ https://raw.githubusercontent.com/VitalPBX/custom-contexts/master/sounds/your-public-ip-is.wav
+wget -P /var/lib/asterisk/sounds/en/ https://github.com/VitalPBX/custom-contexts/raw/master/sounds/your-public-ip-is.wav
 
 rm -f /etc/asterisk/ombutel/extensions__90-custom.conf
 wget -P /etc/asterisk/ombutel/ https://raw.githubusercontent.com/VitalPBX/custom-contexts/master/extensions__90-custom.conf
