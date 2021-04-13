2d1fe5d1fd0c79a97914da5ad3952e91fc680099
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2021-04-13 09:10:17 -0600

    Update cc-install.sh

--- cc-install.sh
+++ cc-install.sh
@@ -11,8 +11,8 @@ rm -rf /var/cache/yum
 rm -f /var/lib/asterisk/sounds/en/your-public-ip-is.wav
 wget -P /var/lib/asterisk/sounds/en/ https://github.com/VitalPBX/custom-contexts/raw/master/sounds/your-public-ip-is.wav
 
-rm -f /etc/asterisk/ombutel/extensions__90-custom.conf
-wget -P /etc/asterisk/ombutel/ https://raw.githubusercontent.com/VitalPBX/custom-contexts/master/extensions__90-custom.conf
+rm -f /etc/asterisk/vitalpbx/extensions__90-custom.conf
+wget -P /etc/asterisk/vitalpbx/ https://raw.githubusercontent.com/VitalPBX/custom-contexts/master/extensions__90-custom.conf
 
 #Intall other required dependencies
 yum install -y asterisk-curl
